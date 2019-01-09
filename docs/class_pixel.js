ec03a3a955ce880511465a9428be84d569a0e627
bendou <sami.dahoux@etu.emse.fr>
2019-01-09 01:25:53 +0100

    code : Update .travis.yml to generate doc

--- docs/class_pixel.js
+++ docs/class_pixel.js
@@ -1,12 +1,6 @@
 var class_pixel =
 [
     [ "Format", "class_pixel.html#aa1531d5d16ab3ab809a8e071145f0a50", [
-      [ "NAlgebra", "group___n_algebra.html#NAlgebra", [
-        [ "NVector", "group___n_algebra.html#NVector", null ],
-        [ "NAlgebra", "group___n_algebra.html#NAlgebra", [
-          [ "NVector", "group___n_algebra.html#NVector", null ]
-        ] ]
-      ] ],
       [ "GScale", "class_pixel.html#aa1531d5d16ab3ab809a8e071145f0a50a0067e32761943fc277951f656686ce1a", null ],
       [ "RGB", "class_pixel.html#aa1531d5d16ab3ab809a8e071145f0a50aff2c7eeaaaaf24a83e97388116481444", null ]
     ] ],
