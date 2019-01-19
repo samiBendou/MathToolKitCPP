d560aa5e7e80ef9bfd57a059190ece4403182f8c
bendou <sbdh75@gmail.com>
2019-01-19 16:59:06 +0100

    code : adding tests

--- TestsMathToolKitCPP/TestNAlgebra/TestPixel.cpp
+++ TestsMathToolKitCPP/TestNAlgebra/TestPixel.cpp
@@ -13,7 +13,6 @@ protected:
         _b = Pixel(10, 11, 12);
         _c = Pixel(Pixel::GScale);
         _d = Pixel(Pixel::RGB);
-
     }
 
     Pixel _a, _b, _c, _d;
@@ -21,20 +20,27 @@ protected:
 
 
 TEST_F(PixelTest, Getters) {
-    ASSERT_EQ(_a.grey(), 10);
-    ASSERT_EQ(_a.red(), 10);
-    ASSERT_EQ(_a.green(), 10);
-    ASSERT_EQ(_a.blue(), 10);
+    EXPECT_EQ(_a.grey(), 10);
+    EXPECT_EQ(_a.red(), 10);
 
+    EXPECT_EQ(_b.grey(), 11);
+    EXPECT_EQ(_b.green(), 11);
 
-    ASSERT_EQ(_b.grey(), 11);
-    ASSERT_EQ(_b.red(), 10);
-    ASSERT_EQ(_b.green(), 11);
-    ASSERT_EQ(_b.blue(), 12);
+    EXPECT_EQ(_c.grey(), 0);
 
-    ASSERT_EQ(_c.grey(), 0);
+    EXPECT_EQ(_d.blue(), 0);
+}
 
-    ASSERT_EQ(_d.red(), 0);
-    ASSERT_EQ(_d.green(), 0);
-    ASSERT_EQ(_d.blue(), 0);
+TEST_F(PixelTest, Setters) {
+    EXPECT_EQ(_c.setRed(4), Pixel(4, 0, 0));
+    EXPECT_EQ(_c.setGrey(5), Pixel(5));
+
+    EXPECT_EQ(_b.setGrey(5), Pixel(5));
+    EXPECT_EQ(_b.setRed(4), Pixel(4, 5, 5));
+}
+
+TEST_F(PixelTest, Limit) {
+    EXPECT_EQ(_a.setGrey(366), Pixel(366));
+    EXPECT_EQ(_a.setLimited(true), Pixel(255));
+    EXPECT_EQ(_a.setLimited(false), Pixel(255));
 }
